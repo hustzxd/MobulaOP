791566a586036b449e11d2d4c11f83787ddf1bf5
wkcn <wkcn@live.cn>
2019-01-11 15:26:51 +0800

    add examples test

---
+++ tests/test_examples.py
@@ -0,0 +1,34 @@
+import mobula
+import importlib
+import runpy
+import os
+import sys
+
+
+def packages_exist(pkg_names):
+    for pkg in pkg_names:
+        try:
+            importlib.import_module(pkg)
+        except ImportError:
+            return False
+    return True
+
+
+def test_examples():
+    EXAMPLES_PATH = './examples'
+    examples = [
+        ([], ['MyFirstOP']),
+        (['mxnet'], ['ConstantOP', 'RunROIAlign',
+                     'dynamic_import_op/dynamic_import_op']),
+    ]
+    sys.path.append('./')
+    for dep_pkgs, examples in examples:
+        if packages_exist(dep_pkgs):
+            for example in examples:
+                print('testing... {}'.format(example))
+                subpath, mod_name = os.path.split(example)
+                fullpath = os.path.join(EXAMPLES_PATH, subpath)
+                old_workpath = os.getcwd()
+                os.chdir(fullpath)
+                runpy.run_module(mod_name, {}, '__main__')
+                os.chdir(old_workpath)
